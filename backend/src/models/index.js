c051cecfa440b6077ad00876432e5725cb343a62
Fadoua Belmokhtar <belmfadwa12@gmail.com>
2025-12-07 01:35:17 +0100

    feat(models): add Share model associations

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,8 +1,8 @@
 const { User } = require('./user');    
 const { File } = require('./file');    
 const { Folder } = require('./folder');
+const Share = require('./share');   
   
-
 // User <-> Files (1:N)
 User.hasMany(File, {
   foreignKey: 'userId',
@@ -36,7 +36,7 @@ File.belongsTo(Folder, {
   as: 'folder'
 });
 
-// Folder <-> Folders  
+// Folder <-> Folders (auto-référence)
 Folder.hasMany(Folder, {
   foreignKey: 'parentId',
   as: 'subfolders',
@@ -47,8 +47,42 @@ Folder.belongsTo(Folder, {
   as: 'parent'
 });
 
+// User <-> Shares (1:N)
+User.hasMany(Share, {
+  foreignKey: 'userId',
+  as: 'shares',
+  onDelete: 'CASCADE'
+});
+Share.belongsTo(User, {
+  foreignKey: 'userId',
+  as: 'User'
+});
+
+// File <-> Shares (1:N)
+File.hasMany(Share, {
+  foreignKey: 'fileId',
+  as: 'shares',
+  onDelete: 'CASCADE'
+});
+Share.belongsTo(File, {
+  foreignKey: 'fileId',
+  as: 'File'
+});
+
+// Folder <-> Shares (1:N)
+Folder.hasMany(Share, {
+  foreignKey: 'folderId',
+  as: 'shares',
+  onDelete: 'CASCADE'
+});
+Share.belongsTo(Folder, {
+  foreignKey: 'folderId',
+  as: 'Folder'
+});
+
 module.exports = {
   User,
   File,
-  Folder
+  Folder,
+  Share
 };
