6f800ae3d03a5765ec48900045bef524779165b9
Fadoua Belmokhtar <belmfadwa12@gmail.com>
2025-12-07 01:34:22 +0100

    feat(models): add Share model for file/folder sharing

---
+++ backend/src/models/share.js
@@ -0,0 +1,48 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const Share = sequelize.define('Share', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  shareToken: {
+    type: DataTypes.STRING(64),  // <-- Changé de CHAR(36) à STRING(64)
+    allowNull: false,
+    unique: true,
+  },
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  fileId: {
+    type: DataTypes.UUID,
+    allowNull: true,
+  },
+  folderId: {
+    type: DataTypes.UUID,
+    allowNull: true,
+  },
+  expiresAt: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  downloadCount: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  maxDownloads: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  }
+}, {
+  timestamps: true,
+  tableName: 'shares'
+});
+
+module.exports = Share;
