6e0a7591e1272322fde644308c57ff7118d78af3
Fadoua Belmokhtar <belmfadwa12@gmail.com>
2025-12-07 02:27:19 +0100

    feat(frontend): add shared file page

---
+++ frontend/src/pages/SharedFile.jsx
@@ -0,0 +1,9 @@
+const SharedFile = () => {
+  return (
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-3xl text-white"> *****************</h1>
+    </div>
+  );
+};
+
+export default SharedFile;
